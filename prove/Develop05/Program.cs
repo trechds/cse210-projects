d250c433e12458705d5b7ccfccbd9307e5d875bb
trechds <trechbusiness@gmail.com>
2024-04-09 22:13:25 -0300

    Eternal Quest - Comment added

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
+        /* 
+        Data Persistence:
+        Using files to save and load goals allows the user to keep a record of their goals even after closing the program.
+
+        Input Validation:
+        The program validates user input, ensuring that only valid data is accepted to avoid errors.
+
+        Goal Update Feedback:
+        When you update a goal, the program provides immediate feedback, including a confirmation message and a display of the updated goal.
+
+        Possibility of Continuation:
+        The ability to save and load goals allows the user to resume their progress and continue working toward their goals over time.
+
+        Encapsulation and Modularity:
+        The program is organized into well-defined classes and methods, promoting clean, readable and modular code 
+        to facilitate maintenance and future extension of the system.
+
+        Polymorphism:
+        By leveraging polymorphism, different types of goals (simple, eternal, checklist) can be handled uniformly 
+        through method substitution. Each goal type implements specific behaviors (e.g. display format, update logic) tailored to its nature.
+        */
+
         Console.Clear(); 
         Console.WriteLine("Goal Tracker");
         Console.WriteLine();
